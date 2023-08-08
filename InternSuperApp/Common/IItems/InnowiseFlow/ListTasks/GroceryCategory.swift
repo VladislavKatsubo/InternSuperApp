f563aa2e527c5e945904e29437e94ba5caca9195
Vladislav Katsubo <vladislav.katsubo@gmail.com>
2023-08-08 12:54:05 +0300

    Intent fix

--- InternSuperApp/Common/IItems/InnowiseFlow/ListTasks/GroceryCategory.swift
+++ InternSuperApp/Common/IItems/InnowiseFlow/ListTasks/GroceryCategory.swift
@@ -17,52 +17,34 @@ enum GroceryCategory: CaseIterable {
 
     var title: String {
         switch self {
-        case .fruits:
-            return "Fruits"
-        case .vegetables:
-            return "Vegetables"
-        case .dairy:
-            return "Dairy"
-        case .meats:
-            return "Meats"
-        case .fish:
-            return "Fish"
-        case .grains:
-            return "Grains"
+        case .fruits: return "Fruits"
+        case .vegetables: return "Vegetables"
+        case .dairy: return "Dairy"
+        case .meats: return "Meats"
+        case .fish: return "Fish"
+        case .grains: return "Grains"
         }
     }
 
     var iconImage: UIImage {
         switch self {
-        case .fruits:
-            return UIImage(systemName: "apple.logo") ?? UIImage()
-        case .vegetables:
-            return UIImage(systemName: "carrot") ?? UIImage()
-        case .dairy:
-            return UIImage(systemName: "drop.fill") ?? UIImage()
-        case .meats:
-            return UIImage(systemName: "hare") ?? UIImage()
-        case .fish:
-            return UIImage(systemName: "fish") ?? UIImage()
-        case .grains:
-            return UIImage(systemName: "leaf") ?? UIImage()
+        case .fruits: return UIImage(systemName: "apple.logo") ?? UIImage()
+        case .vegetables: return UIImage(systemName: "carrot") ?? UIImage()
+        case .dairy: return UIImage(systemName: "drop.fill") ?? UIImage()
+        case .meats: return UIImage(systemName: "hare") ?? UIImage()
+        case .fish: return UIImage(systemName: "fish") ?? UIImage()
+        case .grains: return UIImage(systemName: "leaf") ?? UIImage()
         }
     }
 
     var backgroundColor: UIColor {
         switch self {
-        case .fruits:
-            return .systemOrange
-        case .vegetables:
-            return .systemGreen
-        case .dairy:
-            return .systemBrown
-        case .meats:
-            return .systemRed
-        case .fish:
-            return .systemBlue
-        case .grains:
-            return .systemYellow
+        case .fruits: return .systemOrange
+        case .vegetables: return .systemGreen
+        case .dairy: return .systemBrown
+        case .meats: return .systemRed
+        case .fish: return .systemBlue
+        case .grains: return .systemYellow
         }
     }
 }
